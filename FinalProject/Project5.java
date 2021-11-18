78d3b636a6956a897de084dc250de0994aff867c
Cpoyer <cpoyer@mail.umw.edu>
2021-11-18 11:53:15 -0500

    Create Project5.java

---
+++ FinalProject/Project5.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Project5 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Project5
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args)
+    {
+        System.out.println("My first github practice.");
+    }
+    }
+
