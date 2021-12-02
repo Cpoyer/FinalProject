aec5a9215078b336f00747fb7ef0454652a17baf
Cpoyer <cpoyer@mail.umw.edu>
2021-12-01 21:08:49 -0500

    Scrapped this + rewrote
    DO NOT USE!!

--- FinalProject/Project5.java
+++ FinalProject/Project5.java
@@ -8,48 +8,57 @@ import java.util.Scanner;
 public class Project5
 {
     // instance variables - replace the example below with your own
-    Scanner scnr = new Scanner(System.in);
 
-    public Project5() throws InvalidChoiceException {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
         System.out.println("You find yourself outside of a spooky mansion!");
         System.out.println("You enter the mansion and see three paths.");
         System.out.println("You can enter the study, the basement, or the kitchen.");
         System.out.println("Where would you like to go?");
         String choice1 = scnr.nextLine();
-
         if(choice1.equals("Study")) {
-         return studyStory;
+            studyStory();
         } else if(choice1.equals("Basement")) {
-         return basementStory;
+            basementStory();
         } else if(choice1.equals("Kitchen")) {
-         return kitchenStory;
+            kitchenStory();
         } else {
-            throw new InvalidChoiceException("You died! Try again!");
+          ///
         }
     }
-    public void studyStory() {
+
+    public static String studyStory() {
+        Scanner scnr = new Scanner(System.in);
         System.out.println("You see a table with a book and a candle on it.");
         System.out.println("What do you touch?");
         String studyChoice = scnr.nextLine();
         if(studyChoice.equals("Book")) {
             System.out.println("You start reading, failing to notice a ghost appearing behind you.");
-            System.out.println("It spooks you!");
-            //throw exception
+            System.out.println("How many pages do you read?");
+            
         } else if(studyChoice.equals("Candle")) {
             System.out.println("You pick up the candle, using it to illuminate the room.");
             System.out.println("You find a really cool statue hidden in the corner.");
-            
+
         }
+
     }
-    public void basementStory() {
+
+    public static String basementStory() {
+        Scanner scnr = new Scanner(System.in);
         System.out.println("You stumble into the basement, seeing nothing in the dark.");
         System.out.println("You can either reach to your right, or your left.");
         System.out.println("What direction to you reach towards?");
         String basementChoice = scnr.nextLine();
+        int count = 0;
         if(basementChoice.equals("Right")) {
             System.out.println("You reach towards your right, and brush up against a cobweb!");
-            System.out.println("You are deathly afraid of spiders, so you start to freak out!");
-            //throw exception
+            System.out.println("You hear something approaching in the dark!");
+            while(count > 6) {
+                System.out.println(count);
+                count = count + 1;
+            }
+            System.out.println("A ghost appears and scares you!");
         }
         if(basementChoice.equals("Left")) {
             System.out.println("You reach towards your left, bumping into a light switch.");
@@ -58,7 +67,9 @@ public class Project5
             System.out.println("You decide to take several of the bottles, selling them on Ebay and making a fortune.");
         }
     }
-    public void kitchenStory() {
+
+    public static String kitchenStory() {
+        Scanner scnr = new Scanner(System.in);
         System.out.println("You walk into the old kitchen, finding yourself very hungry.");
         System.out.println("You decide to make yourself a snack with what,s available.");
         System.out.println("You can make a sandwich, some soup, or some cereal.");
@@ -68,20 +79,19 @@ public class Project5
             System.out.println("You start to make yourself a peanut butter and jelly sandwich.");
             System.out.println("However, you fail to see that the bread you use is moldy.");
             System.out.println("Fifteen minutes later, you die due to accute food poisoning.");
-            //throw exception
         }
-        if(kitchenChoice.equals("Soup")) {
+        else if (kitchenChoice.equals("Soup")) {
             System.out.println("You reheat some tomato soup you find in a cabinet.");
             System.out.println("You wish that the soup was chicken noodle.");
             System.out.println("But free food is free food, so you don't complain.");
             System.out.println("All in all, you enjoy your meal.");
         }
-        if(kitchenChoice.equals("Cereal")) {
+        else if(kitchenChoice.equals("Cereal")) {
             System.out.println("You find a box of raisin bran in the cabinet.");
             System.out.println("Searching around, however, you fail to find any milk.");
             System.out.println("You decide to rough it and eat the cereal raw.");
             System.out.println("After breaking your teeth on the stale bran, you regret your decision.");
-            //throw exception
-        }
+        } else
+            System.out.println("Try again!");
     }
 }
